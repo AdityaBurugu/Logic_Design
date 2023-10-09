4b00a570f33978f7703e0c2927e32422aa47e9f2
Aditya Burugu <65937016+AdityaBurugu@users.noreply.github.com>
2023-10-10 00:56:23 +0530

    Add files via upload

---
+++ Logic Gates/Logic Gates Structural Flow Modeling/AND Gate/andgate_sf.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Aditya Burugu
+// 
+// Create Date: 10/10/2023 12:35:47 AM
+// Design Name: 
+// Module Name: andgate_sf
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module andgate_sf(y,a,b);
+output y;
+input a,b;
+
+wire y;
+
+and (y,a,b);
+endmodule
---
+++ Logic Gates/Logic Gates Structural Flow Modeling/AND Gate/andgate_sf_tb.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Aditya Burugu
+// 
+// Create Date: 10/10/2023 12:39:52 AM
+// Design Name: 
+// Module Name: andgate_sf_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module andgate_sf_tb(); 
+reg a,b; 
+wire y; 
+andgate_sf x1(y,a,b); 
+initial 
+begin 
+{a,b}=2'b00; 
+#10 {a,b}=2'b01; 
+#20 {a,b}=2'b10; 
+#40 {a,b}=2'b11; 
+#60 $finish; 
+end 
+endmodule 
