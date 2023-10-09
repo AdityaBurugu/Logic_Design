607e99a7145c70385ac7f375c3e80f0d901fbebd
Aditya Burugu <65937016+AdityaBurugu@users.noreply.github.com>
2023-10-10 00:32:45 +0530

    Add files via upload

---
+++ Logic Gates/Logic Gates Behavioural Flow Modeling/AND Gate/andgate_bf.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/09/2023 11:20:10 PM
+// Design Name: 
+// Module Name: andgate_bf
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module andgate_bf(y,a,b); 
+output y; 
+input a,b; 
+reg y; 
+always@(a,b) 
+begin 
+if(a==1'b1 & b==1'b1) 
+y=1; 
+else 
+y=0; 
+end 
+endmodule 
+
---
+++ Logic Gates/Logic Gates Behavioural Flow Modeling/AND Gate/andgate_bf_tb.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/09/2023 11:21:20 PM
+// Design Name: 
+// Module Name: andgate_bf_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module andgate_bf_tb(); 
+reg a,b; 
+wire y; 
+andgate_bf x1(y,a,b); 
+initial 
+begin 
+{a,b}=2'b00; 
+#10 {a,b}=2'b01; 
+#20 {a,b}=2'b10; 
+#40 {a,b}=2'b11; 
+#60 $finish; 
+end 
+endmodule 
+
