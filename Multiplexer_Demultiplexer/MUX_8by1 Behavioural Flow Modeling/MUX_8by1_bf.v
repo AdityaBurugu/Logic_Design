bbcde1fd9c33003b735bad7d95f75c93ee8c7bf8
Aditya Burugu <adityaburugu1234567@gmail.com>
2023-11-23 01:54:20 +0530

    Update MUX_8by1_bf.v

--- Multiplexer_Demultiplexer/MUX_8by1 Behavioural Flow Modeling/MUX_8by1_bf.v
+++ Multiplexer_Demultiplexer/MUX_8by1 Behavioural Flow Modeling/MUX_8by1_bf.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Aditya Burugu
 // 
 // Create Date: 11/23/2023 01:29:45 AM
 // Design Name: 
