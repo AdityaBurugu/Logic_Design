27e002895642e4cc076681052693c5d54aa586cd
Aditya Burugu <adityaburugu1234567@gmail.com>
2023-11-26 17:13:13 +0530

    Update Binary_Gray_Converter_sf_tb.v

--- Code Converters/Binary to Gray Structural Flow Modeling/Binary_Gray_Converter_sf_tb.v
+++ Code Converters/Binary to Gray Structural Flow Modeling/Binary_Gray_Converter_sf_tb.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Aditya Burugu
 // 
 // Create Date: 11/18/2023 01:58:05 PM
 // Design Name: 
@@ -42,6 +42,6 @@ begin
 #10 {b[3],b[2],b[1],b[0]}=4'b1101;
 #10 {b[3],b[2],b[1],b[0]}=4'b1110;
 #10 {b[3],b[2],b[1],b[0]}=4'b1111;
-
+#10 $finish;
 end 
 endmodule
